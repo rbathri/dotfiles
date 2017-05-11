8013b1cd58db829025fbad21bfa6cb4206532f09
Bathrinath Raveendran <braveendran@linkedin.com>
2017-05-11 16:17:47 +0530

    disabled ssh config change via setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -f ~/.gitconfig ~/.ssh/config ~/.vimrc ~/.vim ~/.tmux.conf
+#rm -f ~/.gitconfig ~/.ssh/config ~/.vimrc ~/.vim ~/.tmux.conf
 cwd="$(cd "$(dirname "$0")" && pwd)"
 
 ln -s $cwd/gitconfig ~/.gitconfig
 
-mkdir -p ~/.ssh && ln -s $cwd/sshconfig ~/.ssh/config
-mkdir -p ~/.ssh/cm_socket
+#mkdir -p ~/.ssh && ln -s $cwd/sshconfig ~/.ssh/config
+#mkdir -p ~/.ssh/cm_socket
 
 ln -s $cwd/vimrc ~/.vimrc
 ln -s $cwd/dotvim ~/.vim
